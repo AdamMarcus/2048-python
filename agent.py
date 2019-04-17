c7702af9e0a1ab78f8622abb240419b40d5444f2
AdamMarcus <adam.marcus.austin@gmail.com>
2019-04-16 21:39:58 -0600

    Added code to check if a move is valid or not.

--- agent.py
+++ agent.py
@@ -19,6 +19,10 @@ class Agent:
         time.sleep(self.waitTime)
         self.myGrid.key_down_str(key)
 
+    def checkMoveValid(self, move):
+        _, done = self.myGrid.getCommand(move)(self.getCurrMat())
+        return done
+
 
 class RandomAgent(Agent):
     def __init__(self, gGrid, waitTime=0.1):
@@ -34,12 +38,16 @@ class RandomAgent(Agent):
             choice = random.choices([c.KEY_UP_AGENT, c.KEY_DOWN_AGENT, c.KEY_LEFT_AGENT, c.KEY_RIGHT_AGENT], weights=[0.25, 0.25, 0.25, 0.25])[0]
             print("Temp choice: ", choice)
             count += 1
-            _, done = self.myGrid.getCommand(choice)(self.getCurrMat())
+            done = self.checkMoveValid(choice)
 
             # If I have lost
             if count > 4:
                 # do something
                 print("I've lost")
+                #############################
+                print("########Last Resort!!!!!!")
+                self.myGrid.after(20, self.myGrid.task)
+                #############################
                 break
 
         print("Random agent choice: ", choice)
@@ -59,19 +67,39 @@ class PatternAgentULRD(Agent):
         done = False
         # Always try up first
         choice = c.KEY_UP_AGENT
-        _, done = self.myGrid.getCommand(choice)(self.getCurrMat())
-        if not done:
+        if not self.checkMoveValid(choice):
             # Always try left second
             choice = c.KEY_LEFT_AGENT
-            _, done = self.myGrid.getCommand(choice)(self.getCurrMat())
-            if not done:
+            if not self.checkMoveValid(choice):
                 # Always try right third
                 choice = c.KEY_RIGHT_AGENT
-                _, done = self.myGrid.getCommand(choice)(self.getCurrMat())
-                if not done:
+                if not self.checkMoveValid(choice):
                     # Always try left last
                     choice = c.KEY_DOWN_AGENT
-                    _, done = self.myGrid.getCommand(choice)(self.getCurrMat())
+        print("Random agent choice: ", choice)
+        self.pressKey(choice)
+
+class PatternAgentLURD(Agent):
+    def __init__(self, gGrid, waitTime=0.1):
+        #super(RandomAgent, self)._init__(self, waitTime=waitTime)
+        self.waitTime = waitTime
+        self.myGrid = gGrid
+        # self.keyboard = Controller()
+
+    def promptAgent(self):
+
+        done = False
+        # Always try up first
+        choice = c.KEY_LEFT_AGENT
+        if not self.checkMoveValid(choice):
+            # Always try left second
+            choice = c.KEY_UP_AGENT
+            if not self.checkMoveValid(choice):
+                # Always try right third
+                choice = c.KEY_RIGHT_AGENT
+                if not self.checkMoveValid(choice):
+                    # Always try left last
+                    choice = c.KEY_DOWN_AGENT
         print("Random agent choice: ", choice)
         self.pressKey(choice)
 
