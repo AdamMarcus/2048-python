c6f05752537f1b78dea96a1464d5b47eddbad0f0
AdamMarcus <adam.marcus.austin@gmail.com>
2019-05-01 10:23:43 -0600

    Testing various methods of training.

--- driver.py
+++ driver.py
@@ -8,8 +8,10 @@ import matplotlib.pyplot as plt
 
 def main():
     # runner = Runner(2, 100, 0)
-    # runner = TrainingWholeCountRunner(2, 100, 0, 10)
-    runner = TrainWholePercentRunner(10, 100, 4, .01)
+    # runner = TrainingWholeCountRunner(10, 100, 4, 20)
+    # runner = TrainingPartialCountRunner(10, 100, 4, 10)
+    #runner = TrainWholePercentRunner(10, 100, 4, .01)
+    runner = TrainPartialPercentRunner(10, 100, 4, .01)
     runner.runTraining()
 
     # runTraining(25, 100, .05)
@@ -103,7 +105,41 @@ class TrainWholePercentRunner(Runner):
 
         return self._trainingRecord
 
+class TrainPartialPercentRunner(Runner):
+    def __init__(self, numEpochs, numItterations, agentCode, percent):
+        super().__init__(numEpochs, numItterations, agentCode)
+        self._percent = percent
 
+
+    def runTraining(self):
+        for epochNum in range(0, self._numEpochs):
+            epochTrainSet = []
+            for itterNum in range(0, self._numItterations):
+
+                self.refreshGameGrid()
+
+                print("Epoch: ", epochNum, " Iteration: ", itterNum)
+                self._gamegrid.mainloop()
+
+                # print(gamegrid.matrix)
+                print("Score: ", self._gamegrid.scoreMatrix())
+                self._agent.setScore(self._gamegrid.scoreMatrix())
+
+                # The current code running AI games needs to know the current epochNum for encoding filename
+                (boards, moves, score) = self._agent.getGameRecord()
+                self._trainingRecord.append((epochNum, itterNum, boards, moves, score))
+                epochTrainSet.append((epochNum, itterNum, boards, moves, score))
+
+            if (self._agentCode == 4):
+                bestGames = getNPercentageBestGames(self._percent, epochTrainSet.copy())
+                self._agent = DNNAgent(None, waitTime=0, trainData=bestGames)
+        with open(('train{}.pickle'.format(randint(10000, 99999))), 'wb') as f:
+            pickle.dump(self._trainingRecord, f)
+            print("Train data stored in {}".format(f))
+
+        plotTrainingRecord(self._trainingRecord)
+
+        return self._trainingRecord
 
 class TrainingWholeCountRunner(Runner):
     def __init__(self, numEpochs, numItterations, agentCode, trainingCount):
@@ -137,8 +173,37 @@ class TrainingWholeCountRunner(Runner):
 
         return self._trainingRecord
 
+class TrainingPartialCountRunner(Runner):
+    def __init__(self, numEpochs, numItterations, agentCode, trainingCount):
+        super().__init__(numEpochs, numItterations, agentCode)
+        self._trainingCount = trainingCount
 
+    def runTraining(self):
+        for epochNum in range(0, self._numEpochs):
+            for itterNum in range(0, self._numItterations):
+                self.refreshGameGrid()
 
+                print("Epoch: ", epochNum, " Iteration: ", itterNum)
+                self._gamegrid.mainloop()
+
+                # print(gamegrid.matrix)
+                print("Score: ", self._gamegrid.scoreMatrix())
+                self._agent.setScore(self._gamegrid.scoreMatrix())
+
+                # The current code running AI games needs to know the current epochNum for encoding filename
+                (boards, moves, score) = self._agent.getGameRecord()
+                self._trainingRecord.append((epochNum, itterNum, boards, moves, score))
+
+            if (self._agentCode == 4):
+                bestGames = getNBestGames(self._trainingCount, self._trainingRecord.copy())
+                self._agent = DNNAgent(None, waitTime=0, trainData=bestGames)
+        with open(('train{}.pickle'.format(randint(10000, 99999))), 'wb') as f:
+            pickle.dump(self._trainingRecord, f)
+            print("Train data stored in {}".format(f))
+
+        plotTrainingRecord(self._trainingRecord)
+
+        return self._trainingRecord
 
 # Data is in the form: (epochNum, itterNum, boards, moves, score)
 def getNPercentageBestGames(nPerc, gameData):
