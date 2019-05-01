120a81b8a634dd6759a9fc71891c8c6993ce74d3
AdamMarcus <adam.marcus.austin@gmail.com>
2019-05-01 08:25:02 -0600

    Added some arguments around which agent to run.

--- driver.py
+++ driver.py
@@ -7,7 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    runTraining(25, 100, .05)
+    # runTraining(25, 100, .05)
+    # wederunTraining(25, 1000, .05)
+    runTraining(25, 100, .01, 4)
     # runTraining(10, 1000, 10, gamegrid, PatternAgentULRD(gamegrid, waitTime=0))
     # runTraining(10, 1000, 10, gamegrid, PatternAgentLURD(gamegrid, waitTime=0))
     # runTraining(10, 1000, 10, gamegrid, ManualAgent(gamegrid, waitTime=0))
@@ -15,19 +17,26 @@ def main():
 
 
 # method to run epochs and iterations to train model
-def runTraining(_numEpochs, _numItterations, _trainingSetPercent):
+# agent codes: 0 = random, 1 = ULRD, 2 = LURD, 3 = Manual, 4 = DNN
+def runTraining(_numEpochs, _numItterations, _trainingSetPercent, agentCode):
     trainingRecord = []
 
+    agent = None
+    if (agentCode = 4):
+        agent = RandomAgent(gamegrid, waitTime=0)
     for epochNum in range(0, _numEpochs):
         for itterNum in range(0, _numItterations):
             gamegrid = GameGrid()
             gamegrid.hide()
 
-            agent = RandomAgent(gamegrid, waitTime=0)
-            # agent = PatternAgentULRD(gamegrid, waitTime=0)
-            # agent = PatternAgentLURD(gamegrid, waitTime=0)
-            # agent = ManualAgent(gamegrid, waitTime=0)
-            # agent = DNNAgent(gamegrid, waitTime=0, trainName="training_epochs/train.pickle")
+            if (agentCode = 0):
+                agent = RandomAgent(gamegrid, waitTime=0)
+            elif (agentCode = 1):
+                agent = PatternAgentULRD(gamegrid, waitTime=0)
+            elif (agentCode = 2):
+                # agent = PatternAgentLURD(gamegrid, waitTime=0)
+            elif (agentCode = 3):
+                # agent = ManualAgent(gamegrid, waitTime=0)
 
             gamegrid.setAgent(agent)
 
@@ -41,8 +50,9 @@ def runTraining(_numEpochs, _numItterations, _trainingSetPercent):
             # The current code running AI games needs to know the current epochNum for encoding filename
             (boards, moves, score) = agent.getGameRecord()
             trainingRecord.append((epochNum, itterNum, boards, moves, score))
-        bestGames = getNPercentageBestGames(_trainingSetPercent, trainingRecord.copy())
-        agent = DNNAgent(gamegrid, waitTime=0, trainData=bestGames)
+        elif (agentCode = 4):
+            bestGames = getNPercentageBestGames(_trainingSetPercent, trainingRecord.copy())
+            agent = DNNAgent(gamegrid, waitTime=0, trainData=bestGames)
 
     with open(('train{}.pickle'.format(randint(10000, 99999))), 'wb') as f:
         pickle.dump(trainingRecord, f)
